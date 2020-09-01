a851873de0c44e916acc4555a7623f9c57ffdc94
Shiva <ams.sks@outlook.com>
2020-09-01 12:11:32 +0100

    Updated indentation in views.py

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,9 +1,12 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-
+from rest_framework import status
+from rest_framework import viewsets
+#from profiles_api import serializers
 
 class HelloApiView(APIView):
     """Test API View"""
+    serializer_class = serializers.HelloSerializer
 
     def get(self, request, format=None):
         """Returns a list of APIView features"""
@@ -15,3 +18,38 @@ class HelloApiView(APIView):
         ]
 
         return Response({'message': 'Hello!', 'an_apiview': an_apiview})
+
+        def post(self, request):
+            """Create a hello message with our name"""
+
+            serializer = self.serializer_class(data=request.data)
+
+            if serializer.is_valid():
+                name = serializer.validated_data.get('name')
+                message = f'Hello {name}'
+                return Response({'message':message})
+            else:
+                return Response(
+                serializer.errors,
+                status=status.HTTP_400_BAD_REQUEST)
+
+                def put(self, request, pk=None):
+                    """Handle updating an object"""
+                    return Response({'method': 'PATCH'})
+
+                    def delete(self, request, pk=None):
+                        """Delete an object"""
+                        return Response({'method': 'DELETE'})
+
+
+                        class HelloViewSet(viewsets.ViewSet):
+                            """ Test API ViewSet"""
+
+                            def list(self, request):
+                                """Return a hello message"""
+                                a_viewset = [
+                                'Uses actions (list, create, retrieve, update, partial_update)',
+                                'Automatically maps to URLS using Routers',
+                                'Provides more functionality with less code',
+                                ]
+                                return Response({'message': 'Hello', 'a_viewset': a_viewset})
