f9f06913c1eca83c9ce41054a63e70e20cda959d
Shiva <ams.sks@outlook.com>
2020-09-01 14:39:41 +0100

    Added viewset to profiles api

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -53,3 +53,33 @@ class HelloApiView(APIView):
                                 'Provides more functionality with less code',
                                 ]
                                 return Response({'message': 'Hello', 'a_viewset': a_viewset})
+
+                                def create(self, request):
+                                    """Create a new hello message"""
+                                    serializer = self.serializer_class(data=request.data)
+                                     if serializer.is_valid():
+                                         name = serializer.validated_data.get('name')
+                                         message = f'Hello {name}!'
+                                         return Response({'message':message})
+                                     else:
+                                         return Response(
+                                                serializer.errors,
+                                                status=status.HTTP_400_BAD_REQUEST
+                                         )
+
+                                def retrieve(self, request, pk=None)
+                                """Handle getting an object by its ID"""
+                                return Response({'http_method':'GET'})
+
+
+                                def update(self, request, pk=None):
+                                    """Handle updating an object"""
+                                    return Response({'http_method': 'PUT'})
+
+                                def partial_update(self, request, pk=None):
+                                    """Handle updating part of an object"""
+                                    return Response({'http_method': 'PATCH'})
+
+                                def destroy(self, request, pk=None):
+                                    """Handle removing an object"""
+                                  return Response({'http_method': 'DELETE'})
