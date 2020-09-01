fe7bd80e6440762e3d8c09c0699f26cedfc64c35
Shiva <ams.sks@outlook.com>
2020-09-01 12:25:40 +0100

    Updated indentation in views.py

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -2,7 +2,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework import viewsets
-#from profiles_api import serializers
+from profiles_api import serializers
 
 class HelloApiView(APIView):
     """Test API View"""
